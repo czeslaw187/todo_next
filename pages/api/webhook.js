74ff1a7958e354d54bfc612db6b843a9f4b16372
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-12-31 12:53:20 +0000

    changed webhook implementation

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,6 +1,5 @@
 import { buffer } from "micro";
-const Stripe = require('stripe');
-
+import Stripe from "stripe";
 const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY, {
     apiVersion: '2020-08-27'
 });
