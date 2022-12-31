37894eca4b097a2cf3672bc4a349de92ae972070
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-12-31 13:20:11 +0000

    changed webhook implementation

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,42 +1,33 @@
-import { buffer } from "micro";
-import Stripe from "stripe";
-const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY, {
-    apiVersion: '2020-08-27'
-});
-const webhookSecret = process.env.NEXT_PUBLIC_STRIPE__WEBHOOK_KEY;
+import initStripe from 'stripe'
+import {buffer} from 'micro'
+import axios from 'axios'
 
-export const config = {
-    api: {
-        bodyParser: false,
-    },
-};
+export const config = {api: {bodyParser: false}}
 
+async function handler(req, res) {
+    console.log(req.body, 'hook')
+    const stripe = initStripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY)
+    const signature = req.headers["stripe-signature"]
+    const signingSecret = process.env.NEXT_PUBLIC_STRIPE__WEBHOOK_KEY
+    let reqBuffer = await buffer(req)
+    reqBuffer = reqBuffer.toString()
 
-const handler = async (req, res) => {
-    if (req.method === "POST") {
-        const buf = await buffer(req);
-        const sig = req.headers["stripe-signature"];
+    let event;
 
-        let stripeEvent;
-
-        try {
-            stripeEvent = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
-            console.log( 'stripeEvent', stripeEvent );
-        } catch (err) {
-            res.status(400).send(`Webhook Error: ${err.message}`);
-            return;
-        }
-
+    try{
+        event = stripe.webhooks.constructEvent(reqBuffer, signature, signingSecret)
         if ( 'checkout.session.completed' === stripeEvent.type ) {
             const session = stripeEvent.data.object;
-            console.log( 'payment success', session );
-       }
-
-        res.json({ received: true });
-    } else {
-        res.setHeader("Allow", "POST");
-        res.status(405).end("Method Not Allowed");
+            console.log( 'payment success', session )
+        } else {
+            console.log('Failure')
+        }
+    } catch(e) {
+        console.log(e)
+        return res.status(400).send(`Webhook error: ${e.message}`)
     }
-};
+    
+    res.send({received: true})
+}
 
-export default handler;
+export default handler
