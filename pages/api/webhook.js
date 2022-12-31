cf01fdebeb88c17f8b4cce0e78e972896676e379
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-12-31 22:41:19 +0000

    changed webhook implementation

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,33 +1,28 @@
-import initStripe from 'stripe'
-import {buffer} from 'micro'
-import axios from 'axios'
+import axios from 'axios';
+const stripe = require('stripe')(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+const endpointSecret = process.env.NEXT_PUBLIC_STRIPE_SIGNING_SECRET;
 
-export const config = {api: {bodyParser: false}}
-
-async function handler(req, res) {
-    console.log(req.body, 'hook')
-    const stripe = initStripe(process.env.NEXT_PUBLIC_STRIPE_WEBHOOK_KEY)
-    const signature = req.headers["stripe-signature"]
-    const signingSecret = process.env.NEXT_PUBLIC_STRIPE_SIGNING_SECRET
-    let reqBuffer = await buffer(req)
-    reqBuffer = reqBuffer.toString()
-
-    let event;
-
-    try{
-        event = stripe.webhooks.constructEvent(reqBuffer, signature, signingSecret)
-        if ( 'checkout.session.completed' === event.type ) {
-            const session = event.data.object;
-            console.log( 'payment success', session );
-        } else {
-            console.log('Failure')
-        }
-    } catch(e) {
-        console.log(e)
-        return res.status(400).send(`Webhook error: ${e.message}`)
+axios.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+  let event = request.body;
+  if (endpointSecret) {
+    const signature = request.headers['stripe-signature'];
+    try {
+      event = stripe.webhooks.constructEvent(
+        request.body,
+        signature,
+        endpointSecret
+      );
+    } catch (err) {
+      console.log(`⚠️  Webhook signature verification failed.`, err.message);
+      return response.sendStatus(400);
     }
-    
-    res.send({received: true})
-}
+  }
 
-export default handler
+  if (event.type === "charge.succeeded") {
+    const charge = event.data.object;
+    response.json(charge)
+  } else {
+    console.warn(`Unhandled event type: ${event.type}`);
+  }
+  response.send();
+});
