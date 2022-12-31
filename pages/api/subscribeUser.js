34e9a61e8159214fb293f20250275ed13e1f4d48
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-12-31 12:26:11 +0000

    fetching users

--- pages/api/subscribeUser.js
+++ pages/api/subscribeUser.js
@@ -1,10 +1,13 @@
 import { Client } from "pg"
+import axios from "axios"
 
 export default async function(req, res) {
     const {email} = req.body
     const client = new Client(process.env.NEXT_PUBLIC_COCKROACHDB_URL)
     await client.connect()
     try {
+        let users = axios.get(process.env.NEXT_PUBLIC_URL + '/api/getAllUsers')
+        console.log(users)
         const duration = Date.now() + 30*86400000
         await client.query('UPDATE users SET subscription=$1 WHERE email=$2',[duration, email])
     } catch (error) {
