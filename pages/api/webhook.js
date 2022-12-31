14fa2692d67cb9b4d80ccda63dc976df205ed81c
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-12-31 22:50:24 +0000

    changed webhook implementation

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,28 +1,33 @@
-import axios from 'axios';
-const stripe = require('stripe')(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
-const endpointSecret = process.env.NEXT_PUBLIC_STRIPE_SIGNING_SECRET;
+import initStripe from 'stripe'
+import {buffer} from 'micro'
+import axios from 'axios'
 
-axios.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
-  let event = request.rawBody;
-  if (endpointSecret) {
-    const signature = request.headers['stripe-signature'];
-    try {
-      event = stripe.webhooks.constructEvent(
-        request.body,
-        signature,
-        endpointSecret
-      );
-    } catch (err) {
-      console.log(`⚠️  Webhook signature verification failed.`, err.message);
-      return response.sendStatus(400);
+export const config = {api: {bodyParser: false}}
+
+async function handler(req, res) {
+    console.log(req.body, 'hook')
+    const stripe = initStripe(process.env.STRIPE_SECRET_KEY)
+    const signature = req.headers["stripe-signature"]
+    const signingSecret = process.env.STRIPE_SIGNING_SECRET
+    let reqBuffer = await buffer(req)
+    reqBuffer = reqBuffer.toString()
+
+    let event;
+
+    try{
+        event = stripe.webhooks.constructEvent(reqBuffer, signature, signingSecret)
+        if (event.type === "charge.succeeded") {
+            const charge = event.data.object;
+            res.json(charge)
+        } else {
+            console.log('Failure')
+        }
+    } catch(e) {
+        console.log(e)
+        return res.status(400).send(`Webhook error: ${e.message}`)
     }
-  }
+    
+    res.send({received: true})
+}
 
-  if (event.type === "charge.succeeded") {
-    const charge = event.data.object;
-    response.json(charge)
-  } else {
-    console.warn(`Unhandled event type: ${event.type}`);
-  }
-  response.send();
-});
+export default handler
